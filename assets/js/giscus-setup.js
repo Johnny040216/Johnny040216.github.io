7d18f5214b535ea2be15eec0525fe00a534beb0f
Johnny040216 <143364746+Johnny040216@users.noreply.github.com>
2025-10-07 09:55:24 +0000

    Deploying to gh-pages from @ Johnny040216/Johnny040216.github.io@2c3706421a7be37412e6a40131c469e305e3b8d7 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Johnny040216/123.github.io",
+    "data-repo": "Johnny040216/Johnny040216.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
